958ce76e4f29cbe20464ce7be1024000d2756b92
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-12-05 13:29:53 -0700

    Setup try catch

--- controllers/news/news-api.js
+++ controllers/news/news-api.js
@@ -5,17 +5,17 @@ module.exports = {
 };
 
 async function topStories(req, res) {
-  console.log("function being called");
   const newsUrl = new URL("https://newsapi.org/v2/top-headlines?");
   const params = new URLSearchParams({
     language: "en",
     country: "ca",
     apiKey: process.env.REACT_APP_NEWS_KEY,
   });
-  console.log(`${newsUrl}${params}`);
-  const response = await fetch(`${newsUrl}${params}`);
-  const body = await response.json();
-  console.log(response);
-  console.log(body);
-  res.json(body);
+  try {
+    const response = await fetch(`${newsUrl}${params}`);
+    const body = await response.json();
+    res.json(body);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 }
