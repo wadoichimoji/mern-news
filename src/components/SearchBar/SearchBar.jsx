44a5890b96d2ecddee49b1d47213d3e5b8b452e2
Lucas <lucas.friedmann35@live.ca>
2022-12-05 15:43:23 -0500

    create search page and import searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <Paper
+      component="form"
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+    >
+      <InputBase
+        fullWidth
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search News Stories"
+      />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 }
